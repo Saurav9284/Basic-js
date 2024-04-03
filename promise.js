84b020211eb4b7a282c7c7f40a8d7e5173e19675
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-04-03 09:50:18 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -9,3 +9,26 @@ promise.then((result) => {
 }).catch((error) => {
   console.error(error);
 });
+
+
+//Promise.all
+
+Promise.all([promise1, promise2, promise3])
+  .then(values => {
+    console.log(values); // Array of resolved values
+  })
+  .catch(error => {
+    console.error(error); // If any of the promises fail
+  });
+
+//Promise.race
+
+Promise.race([promise1, promise2, promise3])
+  .then(value => {
+    console.log(value); // Value of the first resolved promise
+  })
+  .catch(error => {
+    console.error(error); // If the first resolved promise rejects
+  });
+
+
